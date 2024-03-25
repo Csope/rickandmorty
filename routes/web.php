9436c5f9f3b3b84a2530707c0e3e2ec61da88c6f
Csorsz Peter <csorsz.peter@fotex.net>
2024-03-25 21:57:38 +0100

    first version

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EpisodeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [EpisodeController::class, 'index'])->name('episodes');
+Route::post('/filter-episodes', [EpisodeController::class, 'filter']);
+Route::get('/sort-episodes/{name}', [EpisodeController::class, 'sort']);
